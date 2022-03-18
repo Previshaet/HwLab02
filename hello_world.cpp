5661700a498c5b9e42cfc9aead6e1756c21f5edb
Previshaet <89978664+Previshaet@users.noreply.github.com>
2022-03-18 19:06:45 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main()
 	string name;
-	std::cin >> name; //получить имя пользователя 
+	std::cin >> name; //получить <имя_пользователя> :D
 	std::cout << "Hello world from " << name << "!"; //вывод
-	return 0;
+	return 0; //работай пж T-T
 }
